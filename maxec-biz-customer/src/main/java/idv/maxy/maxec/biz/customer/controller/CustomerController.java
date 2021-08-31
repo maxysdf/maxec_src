c9600a80af73b5fe9139537ccac82e5a43976f0f
max.chen <max.chen@sti.com.tw>
2021-09-01 07:55:07 +0800

    customer

--- maxec-biz-customer/src/main/java/idv/maxy/maxec/biz/customer/controller/CustomerController.java
+++ maxec-biz-customer/src/main/java/idv/maxy/maxec/biz/customer/controller/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import idv.maxy.maxec.biz.customer.restapi.CustomerRestApi;
 import idv.maxy.maxec.biz.customer.service.CustomerService;
+import idv.maxy.maxec.biz.customer.vo.RegisterParamVO;
 
 @RestController
 public class CustomerController implements CustomerRestApi {
@@ -12,4 +13,17 @@ public class CustomerController implements CustomerRestApi {
 	@Autowired
 	private CustomerService customerService;
 
+	@Override
+	public void register(RegisterParamVO param) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void deregister(String account, String id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
--- maxec-biz/src/main/java/idv/maxy/maxec/biz/customer/restapi/CustomerRestApi.java
+++ maxec-biz/src/main/java/idv/maxy/maxec/biz/customer/restapi/CustomerRestApi.java
@@ -1,5 +1,19 @@
 package idv.maxy.maxec.biz.customer.restapi;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import idv.maxy.maxec.biz.customer.vo.RegisterParamVO;
+
 public interface CustomerRestApi {
+	
+	@PutMapping("/customer")
+	public void register(@RequestBody RegisterParamVO param) throws Exception;
+	
+	@DeleteMapping("/customer")
+	public void deregister(@RequestParam("account") String account, @RequestParam("id") String id) throws Exception;
+	
 	
 }
