16eaf2d05447175c8c6d36777c253311faa34fd7
max.chen <max.chen@sti.com.tw>
2021-08-31 15:59:32 +0800

    biz member

---
+++ maxec-biz-customer/src/main/java/idv/maxy/maxec/biz/product/controller/ProductController.java
@@ -0,0 +1,65 @@
+package idv.maxy.maxec.biz.product.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import idv.maxy.maxec.biz.product.restapi.ProductRestAPI;
+import idv.maxy.maxec.biz.product.service.ProductService;
+import idv.maxy.maxec.biz.product.vo.BrandVO;
+import idv.maxy.maxec.biz.product.vo.CategoryVO;
+import idv.maxy.maxec.biz.product.vo.FindProductListParam;
+import idv.maxy.maxec.biz.product.vo.ProductVO;
+import idv.maxy.maxec.biz.product.vo.TagVO;
+
+@RestController
+public class ProductController implements ProductRestAPI {
+
+	@Autowired
+	private ProductService productService;
+
+	@Override
+	public ProductVO findProductById(String id) {
+		return productService.findById(id);
+	}
+
+	@Override
+	public List<ProductVO> findAllProduct() {
+		return productService.findAll();
+	}
+
+	@Override
+	public ProductVO findProductByAlias(String alias) {
+		return productService.findByAlias(alias);
+	}
+
+	@Override
+	public List<ProductVO> findProductByIds(List<String> ids) {
+		return productService.findByIds(ids);
+	}
+
+	@Override
+	public List<CategoryVO> findAllCategory() {
+		return productService.findAllCategories();
+	}
+
+	@Override
+	public List<BrandVO> findAllBrand() {
+		return productService.findAllBrands();
+	}
+
+	@Override
+	public Map<String, List<TagVO>> listTagGroupByTypes(List<String> types) {
+		return productService.listTagGroupByTypes(types);
+	}
+	
+	
+}
---
+++ maxec-biz-customer/src/test/java/idv/maxy/maxec/biz/product/ProductDaoTest.java
@@ -0,0 +1,96 @@
+package idv.maxy.maxec.biz.product;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import idv.maxy.maxec.biz.product.dao.BrandDao;
+import idv.maxy.maxec.biz.product.dao.CategoryDao;
+import idv.maxy.maxec.biz.product.dao.ProductDao;
+import idv.maxy.maxec.biz.product.model.Brand;
+import idv.maxy.maxec.biz.product.model.Category;
+import idv.maxy.maxec.biz.product.model.Product;
+import idv.maxy.maxec.biz.product.model.ProductCategoryMap;
+import idv.maxy.maxec.biz.product.model.ProductTagMap;
+import idv.maxy.maxec.biz.product.model.Tag;
+
+/**
+ * 
+ * @author Max Chen
+ *
+ */
+@SpringBootTest(webEnvironment=WebEnvironment.NONE)
+@Transactional
+@ActiveProfiles("product")
+public class ProductDaoTest {
+	private static final Logger logger = LoggerFactory.getLogger(ProductDaoTest.class);
+	
+	@Autowired
+	private ProductDao productDao;
+	
+	@Autowired
+	private BrandDao brandDao;
+	
+	@Autowired
+	private CategoryDao categoryDao;
+	
+	
+	@Test
+	public void testSaveProduct() {
+		Product p = new Product();
+		p.setName("test");
+		
+		productDao.save(p);
+	}
+	
+	@Test
+	public void testSaveBrand() {
+		Brand p = new Brand();
+		p.setName("test brand");
+		
+		Product prd = new Product();
+		prd.setName("test bb");
+		prd.setBrand(p);
+		
+		Category cat = new Category();
+		cat.setName("men");
+		categoryDao.save(cat);
+		
+		Tag tag = new Tag();
+		tag.setType("COLOR");
+		tag.setName("Black");
+		tag.setCode("BLACK");
+		
+		ProductCategoryMap pcm = new ProductCategoryMap();
+		pcm.setProduct(prd);
+		pcm.setCategory(cat);
+		prd.getProductCategoryMaps().add(pcm);
+		cat.getProductCategoryMaps().add(pcm);
+		
+		ProductTagMap ptm = new ProductTagMap();
+		ptm.setProduct(prd);
+		ptm.setTag(tag);
+		prd.getProductTagMaps().add(ptm);
+		tag.getProductTagMaps().add(ptm);
+		
+		productDao.save(prd);
+	}
+	
+	@Test
+	public void testFindAllWithRelated() {
+		List<Product> list = productDao.findAllWithRelated();
+		logger.info("size: " + list.size());
+	}
+	
+}
