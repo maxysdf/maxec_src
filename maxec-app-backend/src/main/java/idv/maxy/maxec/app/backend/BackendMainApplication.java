649d5b6cea6908aa4f7963f15b3d7e91c0336668
max.chen <max.chen@sti.com.tw>
2021-09-02 08:37:24 +0800

    update k8s

--- maxec-app-backend/src/main/java/idv/maxy/maxec/app/backend/BackendMainApplication.java
+++ maxec-app-backend/src/main/java/idv/maxy/maxec/app/backend/BackendMainApplication.java
@@ -2,6 +2,7 @@ package idv.maxy.maxec.app.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableFeignClients
 public class BackendMainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendMainApplication.class, args);
